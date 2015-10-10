a6820d92b399a5bcc53c6bdfb60fdae8f4ae31ee
Itamar Reis Peixoto <itamar@ispbrasil.com.br>
2015-10-10 10:05:29 -0300

    add jars files required by jpa

--- LocarImoveis/src/Aplicacao/Principal.java
+++ LocarImoveis/src/Aplicacao/Principal.java
@@ -8,13 +8,14 @@ package Aplicacao;
 /**
  * @author eduardorocha
  */
-
 import Dominio.Anuncio;
 import Dominio.Foto;
 import java.math.BigDecimal;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
 
 public class Principal {
     public static void main(String[] args) {
@@ -24,8 +25,9 @@ public class Principal {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("LocarImoveisPU");
         EntityManager em = emf.createEntityManager();
 	em.getTransaction().begin();
-        Foto f1 = new Foto(1, "c:\fotos\teste.jpg");
+        Foto f1 = new Foto(1, "c:\\fotos\\teste.jpg");
         em.persist(f1);
+        em.merge(f1);
         em.getTransaction().commit();
 
         
